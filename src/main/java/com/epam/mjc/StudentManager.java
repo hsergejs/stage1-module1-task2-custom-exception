bb8a64906716075aec02960c68991913f6a365ec
Serg <project4444@inbox.lv>
2023-07-24 11:53:15 +0300

    added notes on study materials for future reference

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -5,6 +5,7 @@ public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
+  //throwing custom Exception class StudentNotFoundException on method search
     public Student find(long studentID) throws StudentNotFoundException {
         Student currStudent = Student.getValueOf(studentID);
         if (currStudent != null) {
@@ -17,6 +18,7 @@ public class StudentManager {
   public static void main(String[] args) {
     StudentManager manager = new StudentManager();
 
+    //catching custom exception in during actual search
     for (int i = 0; i < IDs.length; i++) {
         try{
           Student student = manager.find(IDs[i]);
--- src/main/java/com/epam/mjc/StudentNotFoundException.java
+++ src/main/java/com/epam/mjc/StudentNotFoundException.java
@@ -1,5 +1,38 @@
 package com.epam.mjc;
 
+//creating custom exception class, with limited exception class IllegalArgumentException
+//more bright using Exception class
+//can be used combined to re-throw Exception (using built in JDK exceptions)
+//example bellow
+/*
+
+//creating custom exception and passing Throwable class
+public class StudentStoreException extends Exception {
+
+  public StudentStoreException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
+
+//throwing exeption
+public void save(Student student) throws StudentStoreException {
+    try {
+        // execute SQL statements..
+    } catch (SQLException ex) {
+        throw new StudentStoreException("Failed to save student", ex);
+    }
+}
+
+//handling exception example
+StudentManager manager = new StudentManager();
+try {
+    manager.save(new Student());
+  } catch (StudentStoreException ex) {
+    System.err.print(ex);
+  }
+ */
+
+
 public class StudentNotFoundException extends IllegalArgumentException{
     public StudentNotFoundException(String msg){
         super(msg);
